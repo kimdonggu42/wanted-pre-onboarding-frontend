a6653b47196ae9e788b8767af65fe0da40113ea7
kimdonggu42 <kdonggoo44@gmail.com>
2023-06-11 15:48:02 +0900

    Feat : 임시 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import GlobalStyle from './assets/globalStyle';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Todo from './pages/Todo';
 
 function App() {
+  const isLogin = true;
+
   return (
     <div className="App">
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/todo" element={<Todo />} />
+        <Route
+          path="/"
+          element={
+            isLogin ? <Todo /> : <Navigate to="/signin" replace={true} />
+          }
+        />
+        <Route
+          path="/signin"
+          element={isLogin ? <Navigate to="/" replace={true} /> : <Signin />}
+        />
+        <Route
+          path="/signup"
+          element={isLogin ? <Navigate to="/" replace={true} /> : <Signup />}
+        />
       </Routes>
     </div>
   );

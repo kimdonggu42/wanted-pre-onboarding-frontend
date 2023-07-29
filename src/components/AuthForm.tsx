55a945016155b7198ade9aab63ff8269abdec63e
kimdonggu42 <kdonggoo44@gmail.com>
2023-07-29 17:10:41 +0900

    Design : AuthForm 컴포넌트의 style도 authStyle 파일로 이동

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,74 +1,5 @@
-import styled from 'styled-components';
+import * as Styled from '../style/authStyle';
 import { AuthFormTypeProps } from '../util/interface';
-
-export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 30px;
-  width: 90vw;
-  max-width: 410px;
-  border-radius: 4px;
-  border: none;
-  border: 1px solid lightgray;
-`;
-
-export const EmailInput = styled.input`
-  font-size: 14px;
-  width: 100%;
-  height: 50px;
-  border-radius: 4px;
-  padding: 10px 8px 10px 8px;
-  border: none;
-  border: 1px solid lightgray;
-
-  &:focus {
-    outline: none;
-  }
-`;
-
-export const PasswordInput = styled.input`
-  font-size: 14px;
-  width: 100%;
-  height: 50px;
-  border-radius: 4px;
-  padding: 10px 8px 10px 8px;
-  margin-top: 10px;
-  border: none;
-  border: 1px solid lightgray;
-
-  &:focus {
-    outline: none;
-  }
-`;
-
-export const SubmitBtn = styled.button`
-  width: 100%;
-  height: 45px;
-  border: none;
-  border-radius: 4px;
-  margin-top: 20px;
-  color: white;
-  font-size: 15px;
-  font-weight: 600;
-  background-color: ${(props) => props.theme.color.mainColor};
-  cursor: pointer;
-
-  &:hover {
-    opacity: 0.8;
-  }
-
-  &:disabled {
-    background-color: lightgray;
-  }
-`;
-
-export const ErrMessage = styled.div`
-  margin-top: 5px;
-  font-size: 14px;
-  color: ${(props) => props.theme.color.errColor};
-`;
 
 function AuthForm({
   formValue,
@@ -108,16 +39,18 @@ function AuthForm({
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <EmailInput
+    <Styled.Form onSubmit={handleSubmit}>
+      <Styled.EmailInput
         data-testid='email-input'
         type='text'
         value={formValue.email}
         placeholder='이메일을 입력해 주세요'
         onChange={onChangeInput}
       />
-      {formValid.emailValid === false && <ErrMessage>{formErrMessage.emailErrMessage}</ErrMessage>}
-      <PasswordInput
+      {formValid.emailValid === false && (
+        <Styled.ErrMessage>{formErrMessage.emailErrMessage}</Styled.ErrMessage>
+      )}
+      <Styled.PasswordInput
         data-testid='password-input'
         type='password'
         value={formValue.password}
@@ -125,16 +58,16 @@ function AuthForm({
         onChange={onChangeInput}
       />
       {formValid.passwordValid === false && (
-        <ErrMessage>{formErrMessage.passwordErrMessage}</ErrMessage>
+        <Styled.ErrMessage>{formErrMessage.passwordErrMessage}</Styled.ErrMessage>
       )}
-      <SubmitBtn
+      <Styled.SubmitBtn
         data-testid={dataTestId}
         type='submit'
         disabled={formValid.emailValid === false || formValid.passwordValid === false}
       >
         {submitBtnName}
-      </SubmitBtn>
-    </Form>
+      </Styled.SubmitBtn>
+    </Styled.Form>
   );
 }
 
--- src/style/authStyle.tsx
+++ src/style/authStyle.tsx
@@ -14,6 +14,75 @@ export const FormName = styled.div`
   margin-bottom: 20px;
 `;
 
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+  width: 90vw;
+  max-width: 410px;
+  border-radius: 4px;
+  border: none;
+  border: 1px solid lightgray;
+`;
+
+export const EmailInput = styled.input`
+  font-size: 14px;
+  width: 100%;
+  height: 50px;
+  border-radius: 4px;
+  padding: 10px 8px 10px 8px;
+  border: none;
+  border: 1px solid lightgray;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const PasswordInput = styled.input`
+  font-size: 14px;
+  width: 100%;
+  height: 50px;
+  border-radius: 4px;
+  padding: 10px 8px 10px 8px;
+  margin-top: 10px;
+  border: none;
+  border: 1px solid lightgray;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const SubmitBtn = styled.button`
+  width: 100%;
+  height: 45px;
+  border: none;
+  border-radius: 4px;
+  margin-top: 20px;
+  color: white;
+  font-size: 15px;
+  font-weight: 600;
+  background-color: ${(props) => props.theme.color.mainColor};
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:disabled {
+    background-color: lightgray;
+  }
+`;
+
+export const ErrMessage = styled.div`
+  margin-top: 5px;
+  font-size: 14px;
+  color: ${(props) => props.theme.color.errColor};
+`;
+
 export const MoveBtn = styled.button`
   font-size: 15px;
   margin-top: 20px;

b1be7f51a8be24642ff82f01d224de05b3285e9e
kimdonggu42 <kdonggoo44@gmail.com>
2023-07-30 17:36:23 +0900

    Refact : useSignin hook에서 불필요한 navigate 제거

--- src/hooks/useSignin.tsx
+++ src/hooks/useSignin.tsx
@@ -1,9 +1,6 @@
-import { useNavigate } from 'react-router-dom';
 import { BASE_API } from '../util/api';
 
 export const useSignin = () => {
-  const navigate = useNavigate();
-
   const signin = async (
     email: string,
     password: string,
@@ -19,7 +16,6 @@ export const useSignin = () => {
         const res = await BASE_API.post(`/auth/signin`, signInForm);
         if (res.status === 200) {
           localStorage.setItem('accessToken', res.data.access_token);
-          navigate('/todo');
           window.location.reload();
         }
       }

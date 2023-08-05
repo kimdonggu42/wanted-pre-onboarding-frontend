1f3d79df5ae1e2b86a9034f8d744f16cf8dad363
kimdonggu42 <kdonggoo44@gmail.com>
2023-08-05 22:34:24 +0900

    Feat: 루트 경로 접근 시 로그인 되어 있다면 /todo 경로로 리다이렉

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Main from './pages/Main';
 import NotFoundError from './pages/NotFoundError';
 import Signin from './pages/Signin';
@@ -6,10 +6,12 @@ import Signup from './pages/Signup';
 import TodoList from './pages/TodoList';
 
 export function Router() {
+  const isLogin: string | null = localStorage.getItem('accessToken');
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main />} />
+        <Route path='/' element={isLogin ? <Navigate to='/todo' /> : <Main />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/signin' element={<Signin />} />
         <Route path='/todo' element={<TodoList />} />
